f3211c78d4a0da0a1111824683ff1affe8614161
d_dao <dung.dao@hotmail.de>
2021-06-17 12:00:02 +0200

    Add PollenItemControllerTest and tested successfully

--- backend/src/test/java/de/pddao/backend/controller/PollenItemControllerTest.java
+++ backend/src/test/java/de/pddao/backend/controller/PollenItemControllerTest.java
@@ -1,4 +1,6 @@
 package de.pddao.backend.controller;
+
+import de.pddao.backend.model.BloomingMonth;
 import de.pddao.backend.model.PollenItem;
 import de.pddao.backend.repository.PollenItemRepository;
 
@@ -35,66 +37,66 @@ class PollenItemControllerTest {
         //GIVEN
 
         //THEN
-        ResponseEntity<PollenItem[]> response = testRestTemplate.getForEntity("http://localhost:" + port + "api/dictionary", PollenItem[].class);
+        ResponseEntity<PollenItem[]> response = testRestTemplate.getForEntity("http://localhost:" + port + "/api/dictionary", PollenItem[].class);
 
         //WHEN
         assertThat(response.getStatusCode(), is(HttpStatus.OK));
         assertThat(response.getBody(), arrayContainingInAnyOrder(
                 PollenItem.builder()
-                        .englishName("rye") // Roggen
-                        .latinName("Secale cereale")
-                        .beginBloomingSeason("May")
-                        .endBloomingSeason("June")
-                        .distribution("The rye lorem upsum").build(),
-
-                PollenItem.builder()
                         .englishName("ragweed") // Ambrosia (auch Beifuß-Ambrosie)
                         .latinName("Ambrosia artemisiifolia")
-                        .beginBloomingSeason("August")
-                        .endBloomingSeason("September")
+                        .beginBloomingSeason(BloomingMonth.AUGUST)
+                        .endBloomingSeason(BloomingMonth.SEPTEMBER)
                         .distribution("Die Ambrosia gehört zu lorem ipsum").build(),
-
-                PollenItem.builder()
-                        .englishName("mugwort") // Gemeiner Beifuß
-                        .latinName("Artemisia vulgaris")
-                        .beginBloomingSeason("July")
-                        .endBloomingSeason("September")
-                        .distribution("Der Beifuß gehört zu lorem ipsum").build(),
-
-                PollenItem.builder()
-                        .englishName("grasses") // Wildwachsende Süßgräser
-                        .latinName("Poaceae")
-                        .beginBloomingSeason("April")
-                        .endBloomingSeason("October")
-                        .distribution("Die Gräser gehören zu lorem ipsum").build(),
 
                 PollenItem.builder()
                         .englishName("earl") // Schwarz-Erle
                         .latinName("Alnus glutinosa")
-                        .beginBloomingSeason("February")
-                        .endBloomingSeason("April")
+                        .beginBloomingSeason(BloomingMonth.FEBRUARY)
+                        .endBloomingSeason(BloomingMonth.APRIL)
                         .distribution("Die Erle gehört zu lorem ipsum").build(),
 
                 PollenItem.builder()
                         .englishName("hazel") // Hasel
                         .latinName("Corylus avellana")
-                        .beginBloomingSeason("January")
-                        .endBloomingSeason("April")
+                        .beginBloomingSeason(BloomingMonth.JANUARY)
+                        .endBloomingSeason(BloomingMonth.APRIL)
                         .distribution("In Mitteleuropa häufig vorkommender Strauch vom Norddeutschen Tiefland bis zu 1400 m Höhe in den Alpen, in Laubwäldern, an Waldrändern oder als Hecken.").build(),
 
                 PollenItem.builder()
-                        .englishName("ash") // Esche
-                        .latinName("Fraxinus excelsior")
-                        .beginBloomingSeason("March")
-                        .endBloomingSeason("May")
-                        .distribution("In Mitteleuropa weit verbreitet, vom Flachland bis zu 1400 m Höhe in den Alpen, bevorzugt feuchte Standorte").build(),
+                        .englishName("mugwort") // Gemeiner Beifuß
+                        .latinName("Artemisia vulgaris")
+                        .beginBloomingSeason(BloomingMonth.JULY)
+                        .endBloomingSeason(BloomingMonth.SEPTEMBER)
+                        .distribution("Der Beifuß gehört zu lorem ipsum").build(),
+
+                PollenItem.builder()
+                        .englishName("grasses") // Wildwachsende Süßgräser
+                        .latinName("Poaceae")
+                        .beginBloomingSeason(BloomingMonth.APRIL)
+                        .endBloomingSeason(BloomingMonth.OCTOBER)
+                        .distribution("Die Gräser gehören zu lorem ipsum").build(),
+
+                PollenItem.builder()
+                        .englishName("rye") // Roggen
+                        .latinName("Secale cereale")
+                        .beginBloomingSeason(BloomingMonth.MAY)
+                        .endBloomingSeason(BloomingMonth.JUNE)
+                        .distribution("The rye lorem upsum").build(),
 
                 PollenItem.builder()
                         .englishName("birch") // Birke
                         .latinName("Betula pendula")
-                        .beginBloomingSeason("March")
-                        .endBloomingSeason("May")
-                        .distribution("Weit verbreiteter Laubbaum, auch gerne als Park- und Alleebaum in Städten verwendet").build()
-                ));
+                        .beginBloomingSeason(BloomingMonth.MARCH)
+                        .endBloomingSeason(BloomingMonth.MAY)
+                        .distribution("Weit verbreiteter Laubbaum, auch gerne als Park- und Alleebaum in Städten verwendet").build(),
+
+                PollenItem.builder()
+                        .englishName("ash") // Esche
+                        .latinName("Fraxinus excelsior")
+                        .beginBloomingSeason(BloomingMonth.MARCH)
+                        .endBloomingSeason(BloomingMonth.MAY)
+                        .distribution("In Mitteleuropa weit verbreitet, vom Flachland bis zu 1400 m Höhe in den Alpen, bevorzugt feuchte Standorte").build()
+        ));
     }
 }
