02dcef500e129ef3e4506eff106f49c0f7d6233b
d_dao <dung.dao@hotmail.de>
2021-06-24 14:03:28 +0200

    Add legend to PollenForecastPage.js

--- frontend/src/pages/PollenForecastPage.js
+++ frontend/src/pages/PollenForecastPage.js
@@ -1,22 +1,26 @@
 import usePollenCount from "../hooks/usePollenCount";
+import styled from "styled-components/macro";
 
 export default function PollenForecastPage() {
   const { pollenCounts } = usePollenCount();
 
   return (
-    <div>
+    <Wrapper>
       <h1>Pollen Forecast</h1>
       <p>
         Here comes your personal pollen forecast for{" "}
-        {pollenCounts[0]?.content[1]?.region_name}!
+        {pollenCounts[0]?.content[1]?.region_name}! Pleaser consider: updates
+        take place daily at 11 a.m.
       </p>
 
-      <table>
+      <form action=""></form>
+
+      <table className="tableStyle">
         <thead>
           <tr>
-            <th>Pflanzenname</th>
-            <th>heute</th>
-            <th>morgen</th>
+            <th>Pollen type</th>
+            <th>Today</th>
+            <th>Tomorrow</th>
           </tr>
         </thead>
         <tbody>
@@ -62,6 +66,21 @@ export default function PollenForecastPage() {
           </tr>
         </tbody>
       </table>
-    </div>
+
+      <section className="section">
+        <p>Legend: </p>
+        <p>0 no pollen count </p>
+        <p>1 low pollen count</p>
+        <p>2 medium pollen count </p>
+        <p>3 high pollen count</p>
+      </section>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+  .tableStyle td {
+    width: 100%;
+    text-align: center;
+  }
+`;
