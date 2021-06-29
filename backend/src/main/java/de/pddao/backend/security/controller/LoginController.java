b2d2c3944139c03be6bc2de866645fbe0223c3d6
d_dao <dung.dao@hotmail.de>
2021-06-29 22:32:13 +0200

    Add JwtUtilsService

--- backend/src/main/java/de/pddao/backend/security/controller/LoginController.java
+++ backend/src/main/java/de/pddao/backend/security/controller/LoginController.java
@@ -1,6 +1,7 @@
 package de.pddao.backend.security.controller;
 
 import de.pddao.backend.security.model.dto.LoginDataDto;
+import de.pddao.backend.security.service.JwtUtilsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -9,21 +10,26 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+
 @RestController
 @RequestMapping("/auth/login")
 public class LoginController {
 
-    private AuthenticationManager authenticationManager;
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtilsService jwtUtilsService;
 
     @Autowired
-    public LoginController(AuthenticationManager authenticationManager) {
+    public LoginController(AuthenticationManager authenticationManager, JwtUtilsService jwtUtilsService) {
         this.authenticationManager = authenticationManager;
+        this.jwtUtilsService = jwtUtilsService;
     }
 
     @PostMapping
     public String login(@RequestBody LoginDataDto loginDataDto) {
-        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginDataDto.getUsername(), loginDataDto.getPassword()));
+        UsernamePasswordAuthenticationToken userNamePasswordData = new UsernamePasswordAuthenticationToken(loginDataDto.getUsername(), loginDataDto.getPassword());
+        authenticationManager.authenticate(userNamePasswordData);
 
-        return "some jwt";
+        return jwtUtilsService.createToken(new HashMap<>(), loginDataDto.getUsername());
     }
 }
