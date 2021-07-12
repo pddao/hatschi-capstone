00d038bbd1c3543f19a89328aa19617131ae5def
d_dao <dung.dao@hotmail.de>
2021-07-12 11:46:37 +0200

    Add ignore

--- frontend/src/pages/UserPage.js
+++ frontend/src/pages/UserPage.js
@@ -81,6 +81,7 @@ export default function UserPage() {
 
   useEffect(() => {
     allPollenItemsWatchedByUser();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [watchedPollenItems]);
 
   const handleChange = (event) => {
